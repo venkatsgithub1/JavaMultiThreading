50d91f40a5ac0d96dab7f139605217caa62d960f
Ven <venkatsgithub@gmail.com>
2019-08-08 16:59:24 +0530

    Adding comments to reentrant demo code

--- demo/java/parallel_programming/ReentrantLockDemo.java
+++ demo/java/parallel_programming/ReentrantLockDemo.java
@@ -1,23 +1,31 @@
 package demo.java.parallel_programming;
 
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 class ShopperForRenLockExampleClass extends Thread {
     static int garlicCount, potatoCount = 0;
+    /*
+    pencil is a lock object, specifically reentrant lock.
+    A lock which can be incremented any number of times, but has to be unlocked
+    those many number of times too.
+     */
     static ReentrantLock pencil = new ReentrantLock();
 
     private void addGarlic() {
         pencil.lock();
-        System.out.println("hold count:"+pencil.getHoldCount());
+        // Printing the hold count, i.e. number of times the pencil
+        // object is locked.
+        System.out.println("hold count:" + pencil.getHoldCount());
         garlicCount++;
         pencil.unlock();
     }
 
     private void addPotato() {
+        // .lock issues lock.
         pencil.lock();
         potatoCount++;
         addGarlic();
+        // .unlock issues unlock.
         pencil.unlock();
     }
 
